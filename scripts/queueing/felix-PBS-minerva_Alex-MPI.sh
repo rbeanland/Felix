f945b30c48447a4ede47c6324e99f4770abe785c
Alexander Hubert <phrmfk@auriga.theory.warwick.ac.uk>
2015-03-24 13:18:49 +0000

    Attempting to structure a task array submission script for minerva, for a full weak beam analysis I require a lot of small simulations, with ever increasing weak beams

--- scripts/queueing/felix-PBS-minerva_Alex-MPI.sh
+++ scripts/queueing/felix-PBS-minerva_Alex-MPI.sh
@@ -6,7 +6,7 @@ binREFINE=felixrefine
 
 inpfile=felix.inp
 scafile=felix.sca
-ciffile=felix.cif
+hklfile=felix.hkl
 
 #Name of Job
 #-----------
@@ -17,11 +17,14 @@ Job_Name= #job name here (FS... or FR...)
 #---------------
 #input file directory - point to where all input file(s) are located (at the very least the .sca file)
 # NEEDS TO EXIST
-inputfiledir=$HOME/project/KTP_felix/Felix/GaA_test #example
+inputfiledir=$HOME #rest here
 
 # point this to where the felixsim/refine executable is
 # NEEDS TO EXIST
-binarydir=$HOME/project/KTP_felix/Felix/src
+binarydir=$HOME #rest here
+
+# settings for executable
+exe=${1:felixsim}
 
 # submission directory - point to what you want the PBS working directory to be 
 # Doesn't need to exist
@@ -32,21 +35,19 @@ submitdir=/work/e370/e370/ajmh1001/Archer_test_script
 bashtmpdir=/work/e370/e370/ajmh1001/RUNS/Archer_test_script_temp
 
 
-#Input Arguments - only input arguments required are the number of nodes and walltime 
+#Input Arguments
 #---------------
 # settings for parallel submission
-nodes=${1:-1}
-wtime=${2:-00:01:00} 
+cores=${2:-1}
 JobId=${3:-1}
+wtime=${4:-00:10:00}
+queue=${5:devel}
+InputImages=${6:-1}
 
-let cores=${nodes}
+let ranks=${cores}
+let nodes=${cores}
 let mppwidth=${cores}
 
-#from number of nodes, determine the number of processes
-dummyprocesses=$((${cores}*24))
-processes=${4:-${dummyprocesses}}
-
-let ranks=${processes}
 
 if [ ${ranks} -lt 1 ]
 then
@@ -74,41 +75,40 @@ fi
 #loop starts here for looped input file parameters or CIFs#
 #---------------------------------------------------------#
 
-echo "Submitting jobs for ${cores} cores, ${ranks} MPI ranks, 0 OpenMP threads with maximum wall time ${wtime}"
+echo "Submitting jobs for ${cores} cores, ${ranks} MPI ranks, with maximum wall time ${wtime}, submitted to queue ${queue}"
 
 [ -d ${submitdir} ] || mkdir -p ${submitdir}
 cd ${submitdir}
 job_dir=${submitdir}
 
-#if looped, change job file name to depend on each iteration
-job_file=`printf "${Job_Name}_${ranks}.sh"`
 
-echo "Job file location:" ${job_dir}/${job_file}
+job_file=`printf "$Job_Name}_${JobId}.sh"` 
+
+echo "Job File location:" ${job_dir}/${job_file}
 
 [ -d ${job_dir} ] || mkdir -p ${job_dir}
 
-
-#write PBS scripts
 cat > ${job_dir}/${job_file} << EOD
 #!/bin/bash --login
-##PBS -M a.j.m.hubert@warwick.ac.uk
+#PBS -l pvmem=2000mb
+#PBS -M $USER@warwick.ac.uk
 #PBS -m a
 #PBS -r y
 #PBS -V
-##PBS -k oe
+#PBS -k oe
 #PBS -j oe
 
 #       The jobname
-#PBS -N FelixSim_${ranks}
+#PBS -N ${exe}_${JobId}
 
 #       The total number of parallel tasks for your job.
-#PBS -l select=${cores}
+#PBS -l nodes=${nodes}:ppn=1
 
 #       Specify the wall clock time required for your job.
 #PBS -l walltime=${wtime}
 
 #       Specify which budget account that your job will be charged to.
-#PBS -A e370
+##PBS -A midpluswarwick
   
 # Make sure any symbolic links are resolved to absolute path
 export PBS_O_WORKDIR=\$(readlink -f \$PBS_O_WORKDIR)
@@ -119,51 +119,66 @@ basedir=\$PBS_O_WORKDIR
 echo "basedir=" \${basedir}
 
 # The binary directory is the directory where the scripts are
-echo "binarydir (felixsim/ refine location)= " ${binarydir}
+echo "binarydir=" ${binarydir}
 
 # do the run in a TMPdir for safekeeping
-tmpdir=${bashtmpdir}/`basename ${job_file} .sh`
+tmpdir=$HOME/RUNS/`basename ${job_file} .sh`
+
 [ -d \${tmpdir} ] || mkdir -p \${tmpdir}
+
+# copy the needed binaries
+cp ${binarydir}/${exe} \${tmpdir}/
+cp \${basedir}/${ciffile} \${tmpdir}/
+cp ${scadir}/${scafile} \${tmpdir}/
+cp ${scadir}/${hklfile} \${tmpdir}/
+
+# copy any input images if present; only needed for felixrefine
+echo $InputImages
+for imgno in {1..$InputImages}
+do
+ echo \${imgno}
+ imgfile=felix.00\${imgno}.img
+ cp ${scadir}/\${imgfile} \${tmpdir}/
+done
 
 # construct the input files
 
 cd \${tmpdir}
-echo "Hostname: " $HOSTNAME
-echo "Current Working Directory = "
+echo $HOSTNAME
 pwd
 
 rm -rf $inpfile
 
 touch $inpfile
 
-echo "# Input file for felixsim version :VERSION: Build :BUILD:"               >> $inpfile
+echo "# Input file for felixrefine version :VERSION: Build :BUILD:"            > $inpfile
 echo "# ------------------------------------"                                  >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# ------------------------------------"                                  >> $inpfile
-echo "# felixsim input"                                                        >> $inpfile
+echo "# felixrefine input"                                                     >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# control flags"                                                         >> $inpfile
-echo "IWriteFLAG                = 3"                                           >> $inpfile
-echo "IImageFLAG                = 0"                                           >> $inpfile
+echo "IWriteFLAG                = 1"                                           >> $inpfile
+echo "IImageFLAG                = 012"                                           >> $inpfile
 echo "IOutputFLAG               = 0"                                           >> $inpfile
 echo "IBinorTextFLAG            = 0"                                           >> $inpfile 
 echo "IScatterFactorMethodFLAG  = 0"                                           >> $inpfile
 echo "ICentralBeamFLAG          = 1"                                           >> $inpfile
-echo "IMaskFLAG                 = 1"                                           >> $inpfile
+echo "IMaskFLAG                 = 0"                                           >> $inpfile
 echo "IZolzFLAG                 = 1"                                           >> $inpfile
 echo "IAbsorbFLAG               = 1"                                           >> $inpfile
 echo "IAnisoDebyeWallerFlag     = 0"                                           >> $inpfile
 echo "IBeamConvergenceFLAG      = 1"                                           >> $inpfile
 echo "IPseudoCubicFLAG          = 0"                                           >> $inpfile
-echo "IXDirectionFLAG           = 0"                                           >> $inpfile
+echo "IXDirectionFLAG           = 1"                                           >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# radius of the beam in pixels"                                          >> $inpfile
 echo "IPixelCount               = 64"                                          >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# beam selection criteria"                                               >> $inpfile
-echo "IMinReflectionPool        = 2500"                                        >> $inpfile
-echo "IMinStrongBeams           = 600"                                         >> $inpfile
-echo "IMinWeakBeams             = 0"                                           >> $inpfile
+echo "IMinReflectionPool        = 300"                                        >> $inpfile
+echo "IMinStrongBeams           = 50"                                         >> $inpfile
+echo "IMinWeakBeams             = 5"                                           >> $inpfile
 echo "RBSBMax                   = 0.1"                                         >> $inpfile
 echo "RBSPMax                   = 0.1"                                         >> $inpfile
 echo "RConvergenceTolerance (%) = 1.0"                                         >> $inpfile
@@ -173,25 +188,25 @@ echo "RDebyeWallerConstant      = 0.4668"                                      >
 echo "RAbsorptionPer            = 2.9"                                         >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# microscope settings"                                                   >> $inpfile
-echo "ROuterConvergenceAngle    = 10.0"                                        >> $inpfile
+echo "ROuterConvergenceAngle    = 3.0"                                         >> $inpfile
 echo "RInnerConvergenceAngle    = 0.0"                                         >> $inpfile
-echo "IIncidentBeamDirectionX   = 0"                                           >> $inpfile
+echo "IIncidentBeamDirectionX   = 1"                                           >> $inpfile
 echo "IIncidentBeamDirectionY   = 1"                                           >> $inpfile
-echo "IIncidentBeamDirectionZ   = 0"                                           >> $inpfile
+echo "IIncidentBeamDirectionZ   = 1"                                           >> $inpfile
 echo "IXDirectionX              = 0"                                           >> $inpfile
 echo "IXDirectionY              = -1"                                          >> $inpfile
 echo "IXDirectionZ              = 1"                                           >> $inpfile
-echo "INormalDirectionX         = 0"                                           >> $inpfile
+echo "INormalDirectionX         = 1"                                           >> $inpfile
 echo "INormalDirectionY         = 1"                                           >> $inpfile
-echo "INormalDirectionZ         = 0"                                           >> $inpfile
+echo "INormalDirectionZ         = 1"                                           >> $inpfile
 echo "RAcceleratingVoltage (kV) = 200.0"                                       >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# Image Output Options"                                                  >> $inpfile
 echo ""                                                                        >> $inpfile
-echo "RInitialThickness        = 1400.0"                                       >> $inpfile
-echo "RFinalThickness          = 3000.0"                                       >> $inpfile
-echo "RDeltaThickness          = 20.0"                                         >> $inpfile
-echo "IReflectOut              = 70"                                           >> $inpfile
+echo "RInitialThickness        = 1000.0"                                       >> $inpfile
+echo "RFinalThickness          = 1000.0"                                       >> $inpfile
+echo "RDeltaThickness          = 10.0"                                         >> $inpfile
+echo "IReflectOut              = 1"                                           >> $inpfile
 echo ""                                                                        >> $inpfile
 echo "# felixrefine Input"                                                     >> $inpfile
 echo ""                                                                        >> $inpfile
@@ -225,36 +240,30 @@ echo  ""                                                                       >
 cat $inpfile
 ls -al \${tmpdir}
 
-#copy input files to temp directory
-cp ${inputfiledir}/* \${tmpdir}
-echo "copying input files from ${inputfiledir}"
-echo "to temp directory: \${tmpdir}"
-
-#if looping over ciffile use this statement to copy each individual one to its tmp folder
-#cp \$cifdir/... \${tmpdir}/felix.cif
-
 # Make sure ranks are numbered appropriately
 export MPICH_RANK_REORDER_METHOD=1 
 export MPICH_PTL_MATCH_OFF=1
 export MPICH_FAST_MEMCPY=1
 export IPATH_NO_CPUAFFINITY=1
+  
+# Launch the parallel job, ${ranks} MPI ranks in ${taskspernode} tasks per node with ${ompthreads} threads
+#export OMP_NUM_THREADS=${ompthreads}
 
 echo "--- starting the SIM run"
-aprun -n ${ranks} -N 24 ${binarydir}/felixsim
-
-#echo "---starting the REFINE run"
-#aprun -n ${ranks} -N 24 ${binarydir}/felixrefine
+mpirun ./$exe
 
 echo "--- starting the IMAGE post-processing run"
-echo "Output files to be copied"
+pwd
+ls -l
 
-ls-l
+#convert -size 539x539 -depth 64 -define quantum:format=floating-point -define quantum:scale=65535.0 -endian lsb GRAY:F-WI-M-T01000-P00539-sampleoutput-highq.bin F-WI-M-T01000-P00539-sampleoutput-highq.tif
 
 echo "--- starting the image part"
 
 # copy the result of the run in the detination for safekeeping
 
-targetdir=$HOME/RUNS/`basename ${job_file} .sh`
+targetdir=${submitdir}/`basename ${job_file} .sh`
+
 [ -d \${targetdir} ] || mkdir -p \${targetdir}
 
 cp -vr * \${targetdir}
@@ -270,9 +279,8 @@ EOD
 #job submission arguments, uncomment queue required
 
 chmod 755 ${job_dir}/${job_file}
-#(cd ${job_dir} ; qsub ./${job_file})
-#(cd ${job_dir} ; qsub -q long ./${job_file})
-#(cd ${job_dir} ; qsub -q short ./${job_file})
-#(cd ${job_dir} ; qsub -q low ./${job_file})
 #(cd ${job_dir} ; qsub -q serial ./${job_file})
-
+#(cd ${job_dir} ; qsub -q parallel ./${job_file})
+(cd ${job_dir} ; qsub -q $queue ./${job_file})
+#(cd ${job_dir} ; qsub -q taskfarm ./${job_file})
+#(cd ${job_dir} ; qsub ./${job_file})
